7312f5d87760697fdc7d470f92909c9698dfe546
senayaydemir <senayaydemir00@gmail.com>
2025-06-06 23:30:54 +0300

    alıcı ve verici indexler eklendi

--- uygulama.py
+++ uygulama.py
@@ -1,6 +1,11 @@
+import random
 isimler=["duygu","özge","betül","ali","merve","metin"]
 
 def hediye_cekilisi():
     alanlar=isimler.copy()
-    verenlelr=isimler.copy()
+    verenler=isimler.copy()
     
+    alici_index=random.randint(0,len(alanlar - 1))
+    verici_index=random.randint(0,len(verenler - 1))
+
+    print(alici_index,"Şu kişiye hediye alacak:",verici_index)
